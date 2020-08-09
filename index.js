f54d1f6838ddc6c9e9c745e3af28faaef948eb65
webguybit <xclusive.shakil@live.com>
2020-08-10 01:18:52 +0800

    first version for test

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ function showDiv(year_val) {
     console.log(year_val);
     var display_val =  $(`.${year_val}`).css('display');
     console.log(display_val);
+    var year_4_digit = year_val.slice(1,5);
     if (display_val == 'none'){
         $(`.${year_val}`).css({'display':'block'});
+        $(`#e${year_4_digit}`).css({'background-color':'#5c5761'});
     }
     else{
         $(`.${year_val}`).css({'display':'none'});
+        $(`#e${year_4_digit}`).css({'background-color':'#1a0033'});
     }
 }
